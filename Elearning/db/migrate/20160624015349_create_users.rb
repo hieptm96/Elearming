028634e8f6519cb699afb6b3c06bd4fe71e5cc1f
hieptm96 <hiep1996tb@gmail.com>
2016-06-24 08:58:09 +0700

    Them table User va Category

---
+++ Elearning/db/migrate/20160624015349_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :password
+      t.string :email
+      t.datetime :birthday
+      t.string :address
+      t.number :phone_number
+      t.boolean :admin
+
+      t.timestamps null: false
+    end
+  end
+end
