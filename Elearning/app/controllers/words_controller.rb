1d5207918c15098b84360c92493be094c7353d58
hieptm96 <hiep1996tb@gmail.com>
2016-06-25 14:38:54 +0700

    Update index action in words_controller to filter list words follow learned, not leared, all

--- Elearning/app/controllers/words_controller.rb
+++ Elearning/app/controllers/words_controller.rb
@@ -1,6 +1,16 @@
 class WordsController < ApplicationController
   def index
-  	@words = Word.all
+  	if params['category'] && params['learn']
+  		if params['learn'] == 'all'
+  			@words = Word.find_by_sql ["select * from words where category_id = ?",params['category']]
+  		end
+  		if params['learn'] == 'learned'
+  			@words = Word.find_by_sql ["select * from words where (words.id in (select word_id from results where user_id = ?)) and (category_id = ?)",session[:user_id],params['category']]
+  		end
+  		if params['learn'] == 'notlearned'
+  			@words = Word.find_by_sql ["select * from words where (words.id not in (select word_id from results where user_id = ?)) and (category_id = ?)",session[:user_id],params['category']]
+  		end
+  	end
   end
 
   def new
